1080de9e10ea2aea02c47164c5308cc18141b4c3
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2019-04-06 22:33:36 -0300

    Criada a tabela do dashboard que mostar os anuncios que o usuario realizou.

--- dashboard.php
+++ dashboard.php
@@ -5,6 +5,7 @@ if (!isset($_SESSION['id']) or empty($_SESSION['id'])) {
     header("Location: login.php");
 }
 
+require "conexao.php";
 require "cabecalho.php";
 ?>
 
@@ -27,6 +28,51 @@ require "cabecalho.php";
         <div class="row">
             <div class="col text-center"><h3>Meus Anúncios</h3></div>
         </div>
+
+        <table id="tblDashboard" class="table table-striped">
+            <?php
+                $sql = "SELECT id, produto, descricao, preco, estado " .
+                "FROM anuncios " .
+                "WHERE id_usuario = '" . $_SESSION['id'] . "';";
+
+                $sql = $pdo->query($sql);
+
+                if ($sql->rowCount() > 0) {
+                    ?>
+                    <thead>
+                        <tr>
+                            <th>Anúncio</th>
+                            <th>Descrição</th>
+                            <th>Preço</th>
+                            <th>Estado</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+        
+                    <tbody>
+                    <?php
+                        foreach ($sql->fetchAll() as $anuncio) {
+                            echo "<tr>";
+                            echo "<td>" . $anuncio['produto'] . "</td>";
+                            echo "<td>" . $anuncio['descricao'] . "</td>";
+                            echo "<td>R$ " . $anuncio['preco'] . "</td>";
+                            echo "<td>";
+                                if ($anuncio['estado'] == "novo") {
+                                    echo "Novo";
+                                } else {
+                                    echo "Usado";
+                                }
+                            echo "</td>";
+                            echo "<td><a class='btn btn-secondary' href='anunciar.php?id=" . $anuncio['id'] . "&id_usuario=" . $_SESSION['id'] . "'>Editar</a> <a class='btn btn-danger' href='#'>Excluir</a>";
+                            echo "</tr>";
+                        }
+                } else {
+                    echo "<h5 class='text-dark text-center'>Você ainda não fez nenhum anúncio! Comece já clicando em <a href='anunciar.php'>Anunciar</a>!</h5>";
+                }
+                echo "</tbody>";
+            ?>
+            
+        </table>
     </div>
         
 
